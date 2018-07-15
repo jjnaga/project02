a7217ac0e1ad43213c4e950f08c2ec73e773db5c
Jeremy Nagahama <naga@hawaii.edu>
2018-07-14 15:28:29 -1000

    Create basic template for Car page

--- src/pages/Model.jsx
+++ src/pages/Model.jsx
@@ -4,10 +4,20 @@ import API from "../../api/API";
 const Model = () => {
 	const { header, image, specs, config, aftermarket } = API;
 
+	const data = Object.entries(specs).map(([key, value]) => (
+		<li key={key}>
+			{key}
+			{value}
+		</li>
+	));
+
 	return (
 		<div id="cover">
 			<h1>{header.name}</h1>
 			<img src={image} alt={`${header.name}`} />
+			<div id="content">
+				<ul>{data}</ul>
+			</div>
 		</div>
 	);
 };
