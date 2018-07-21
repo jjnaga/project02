b93e830a6b8a4738b46a0a407491789c300f1bd6
Jeremy Nagahama <naga@hawaii.edu>
2018-07-20 23:07:26 -1000

    Add info for BMW M4 Specs.

--- src/BMW/components/specs.jsx
+++ src/BMW/components/specs.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Specs = () => (
-	<div id="Specs">
-		<p>Oh Hi Mark</p>
+const Specs = ({ specs }) => (
+	<div id="specs">
+		<ul>
+			<li>Horsepower: {specs.horsepower}</li>
+			<li>Torque: {specs.torque}</li>
+			<li>Weight: {specs.weight}</li>
+			<li>0-60: {specs.zeroToSixty}</li>
+			<li>0-10: {specs.zeroToHundred}</li>
+			<li>Quarter Mile: {specs.quarterMile}</li>
+		</ul>
 	</div>
 );
+
+Specs.defaultProps = {
+	specs: PropTypes.bool,
+};
+
+Specs.propTypes = {
+	specs: PropTypes.objectOf({
+		horsepower: PropTypes.number.isRequired,
+		torque: PropTypes.number.isRequired,
+		weight: PropTypes.number.isRequired,
+		zeroToSixty: PropTypes.number.isRequired,
+		zeroToHundred: PropTypes.number.isRequired,
+		quarterMile: PropTypes.number.isRequired,
+	}),
+};
 
 export default Specs;
